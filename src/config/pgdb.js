11d104a89530abd1bfa9cfc1a806170d2b6afd9e
Habibullo <156281751+KHab1bull0@users.noreply.github.com>
2024-07-27 12:55:29 +0500

    Update pgdb.js

--- src/config/pgdb.js
+++ src/config/pgdb.js
@@ -17,6 +17,5 @@ export const pool = new Pool({
     database: DBNAME
 });
 
-// postgresql://texno_ark_owner:OajTpcL6o2fx@ep-lingering-band-a1mfumhm.ap-southeast-1.aws.neon.tech/texno_ark?sslmode=require
 
 
